9a846622d21b552702d637779449fb24e803d84e
Thomas Lin Pedersen <thomasp85@gmail.com>
2017-08-06 21:55:16 +0200

    Add examples to RouteStack demo

--- R/routestack.R
+++ R/routestack.R
@@ -47,6 +47,29 @@
 #'
 #' @export
 #'
+#' @examples
+#' # Create a new stack
+#' routes <- RouteStack$new()
+#'
+#' # Populate it wih routes
+#' first <- Route$new()
+#' first$add_handler('all', '*', function(request, response, keys, ...) {
+#'   message('This will always get called first')
+#'   TRUE
+#' })
+#' second <- Route$new()
+#' second$add_handler('get', '/demo/', function(request, response, keys, ...) {
+#'   message('This will get called next if the request asks for /demo/')
+#'   TRUE
+#' })
+#' routes$add_route(first, 'first')
+#' routes$add_route(second, 'second')
+#'
+#' # Send a request through
+#' rook <- fiery::fake_request('http://example.com/demo/', method = 'get')
+#' req <- reqres::Request$new(rook)
+#' routes$dispatch(req)
+#'
 RouteStack <- R6Class('RouteStack',
     public = list(
         # Methods
