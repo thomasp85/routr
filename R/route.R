4fd1c338a64597f38de5642f977a6387f53e0856
Thomas Lin Pedersen <thomasp85@gmail.com>
2025-09-29 14:22:53 +0200

    First stab at otel

--- R/route.R
+++ R/route.R
@@ -301,14 +301,54 @@ Route <- R6Class(
       }
       handler <- handlerInfo$handler
       keys <- set_names(handlerInfo$values, handlerInfo$keys)
-      continue <- handler(
-        request = request,
-        response = response,
-        keys = keys,
-        ...
+
+      # OpenTelemetry
+      # TODO: Allow server introspection of actual server host and port (network.local.address and network.local.port)
+      # http.response.status_code and http.response.header.<key> can only be set later
+      span <- otel::start_span(
+        handlerInfo$path,
+        options = list(kind = "server"),
+        attributes = list2(
+          http.request.method = method,
+          url.path = request$path,
+          url.scheme = request$protocol,
+          http.route = handlerInfo$path,
+          network.protocol.name = "http",
+          server.port = as.integer(sub("^.*:(.*)$", "\\1", request$host)),
+          url.query = request$querystring,
+          client.address = request$ip,
+          network.protocol.version = 1.1,
+          server.address = sub("^(.*):.*$", "\\1", request$host),
+          user_agent.original = request$headers[["user_agent"]],
+          !!!set_names(
+            request$headers,
+            paste0(
+              "http.request.header.",
+              gsub("_", "-", names(request$headers))
+            )
+          ),
+          !!!set_names(keys, paste0("path.param.", names(keys)))
+        )
       )
+
+      otel::with_active_span(span, {
+        continue <- handler(
+          request = request,
+          response = response,
+          keys = keys,
+          ...
+        )
+      })
+
       if (!promises::is.promising(continue)) {
+        span$set_attribute("http.response.status_code", as.integer(response$status))
+        otel::end_span(span)
         check_bool(continue)
+      } else {
+        continue <- promises::then(continue, function(val) {
+          span$set_attribute("http.response.status_code", as.integer(response$status))
+          otel::end_span(span)
+        })
       }
       continue
     },
@@ -388,6 +428,7 @@ Route <- R6Class(
       reg[wildcard] <- '(.*)'
       reg <- paste0(paste0(reg, collapse = '/'), terminator)
       list(
+        path = path,
         regex = reg,
         n_tokens = n_tokens,
         n_keys = length(keys),
--- R/routr-package.R
+++ R/routr-package.R
@@ -6,3 +6,5 @@
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
+
+otel_tracer_name <- "thomasp85.routr"
