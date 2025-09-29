92df04788b98b1f960ffd9067888487a9a9bc375
Barret Schloerke <barret@posit.co>
2025-09-29 12:36:20 -0400

    Move otel things to `otel.R`

---
+++ R/otel.R
@@ -0,0 +1,94 @@
+otel_tracer_name <- "thomasp85.routr"
+
+get_tracer <- local({
+  tracer <- NULL
+  function() {
+    if (!is.null(tracer)) {
+      return(tracer)
+    }
+    if (testthat__is_testing()) {
+      # Don't cache the tracer in unit tests. It interferes with tracer provider
+      # injection in otelsdk::with_otel_record().
+      return(otel::get_tracer())
+    }
+    tracer <<- otel::get_tracer()
+    tracer
+  }
+})
+
+testthat__is_testing <- function() {
+  identical(Sys.getenv("TESTTHAT"), "true")
+}
+
+
+with_route_ospan <- function(expr, ..., handlerInfo, method, request, keys) {
+
+  # OpenTelemetry
+  # TODO: Allow server introspection of actual server host and port (network.local.address and network.local.port)
+  # http.response.status_code and http.response.header.<key> can only be set later
+  span <- otel::start_span(
+    handlerInfo$path,
+    options = list(kind = "server"),
+    attributes = list2(
+      http.request.method = method,
+      url.path = request$path,
+      url.scheme = request$protocol,
+      http.route = handlerInfo$path,
+      network.protocol.name = "http",
+      server.port = as.integer(sub("^.*:(.*)$", "\\1", request$host)),
+      url.query = request$querystring,
+      client.address = request$ip,
+      network.protocol.version = 1.1,
+      server.address = sub("^(.*):.*$", "\\1", request$host),
+      user_agent.original = request$headers[["user_agent"]],
+      !!!set_names(
+        request$headers,
+        paste0(
+          "http.request.header.",
+          gsub("_", "-", names(request$headers))
+        )
+      ),
+      !!!set_names(keys, paste0("path.param.", names(keys)))
+    )
+  )
+
+
+
+  otel::with_active_span(span, {
+    continue <- handler(
+      request = request,
+      response = response,
+      keys = keys,
+      ...
+    )
+  })
+
+  if (!promises::is.promising(continue)) {
+    span$set_attribute("http.response.status_code", as.integer(response$status))
+    otel::end_span(span)
+    check_bool(continue)
+  } else {
+    continue <- promises::then(continue, function(val) {
+      span$set_attribute("http.response.status_code", as.integer(response$status))
+      otel::end_span(span)
+    })
+  }
+}
+
+
+
+
+
+  # tracer <- get_tracer()
+
+  # is_enabled <- tracer$is_enabled()
+
+  # if (!is_enabled) {
+  #   return(force(expr))
+  # }
+
+# ,
+# tracer = get_tracer()
+# promises::with_ospan_promise_domain({
+
+# })
