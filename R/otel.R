cefb9151f9cd521a9fd3b67bfea85452dc4978b0
Barret Schloerke <barret@posit.co>
2025-10-20 12:42:29 -0400

    Init with hybrid_then for quick debugging

--- R/otel.R
+++ R/otel.R
@@ -21,6 +21,64 @@ testthat__is_testing <- function() {
 }
 
 
+#' @importFrom promises is.promising then
+hybrid_then <- function(expr, on_success = NULL, on_failure = NULL, tee = FALSE) {
+  force(on_success)
+  force(on_failure)
+  force(tee)
+
+  result_is_sync_error <- FALSE
+
+  result <- tryCatch(
+    expr,
+    error = function(e) {
+      # If no `on_failure` callback, re-throw the error
+      if (is.null(on_failure)) {
+        stop(e)
+      }
+
+      # Perform synchronous `on_failure` callback now
+      result_on_error <- on_failure(e)
+      if (tee) {
+        # Re-throw the error
+        stop(e)
+      } else {
+        result_is_sync_error <<- TRUE
+        result_on_error
+      }
+    }
+  )
+
+  if (result_is_sync_error) {
+    # Return synchronous `tee=FALSE` `result_on_error` from above
+    return(result)
+  }
+
+  if (is.promising(result)) {
+    # Return async result
+    # Will handle async success, failure callbacks (and `tee`)
+    result |>
+      then(on_success, on_failure, tee = tee)
+
+  } else {
+    # Peform synchronous `on_success` callback now
+    result_on_success <-
+      if (!is.null(on_success)) {
+        on_success(result)
+      } else {
+        result
+      }
+
+    # Return synchronous result
+    if (tee) {
+      result
+    } else {
+      result_on_success
+    }
+  }
+}
+
+
 with_route_ospan <- function(
   expr,
   ...,
@@ -35,36 +93,25 @@ with_route_ospan <- function(
   is_enabled <- tracer$is_enabled()
 
   if (!is_enabled) {
-    continue <- force(expr)
-    if (!promises::is.promising(continue)) {
-      if (check_output) check_bool(continue, call = call)
-    } else {
-      continue <- promises::then(continue, function(val) {
-        if (check_output) {
-          check_bool(val, call = call)
-        }
-        val
-      })
-    }
+
+    continue <-
+      if (check_output) {
+        hybrid_then(
+          expr,
+          on_success = function(continue) {
+            check_bool(continue, call = call)
+          },
+          tee = TRUE
+        )
+      } else {
+        force(expr)
+      }
+
     return(continue)
   }
 
   name <- paste0(request$method, "_", handlerInfo$path)
-
   parent <- request$otel
-
-  # OpenTelemetry
-  # TODO: Allow server introspection of actual server host and port (network.local.address and network.local.port)
-  # http.response.status_code and http.response.header.<key> can only be set later
-  span <- otel::start_span(
-    paste0(name, "_route"),
-    options = list(kind = "server", parent = parent),
-    attributes = list2(
-      routr.route = handlerInfo$path,
-      !!!set_names(keys, paste0("routr.path.param.", names(keys)))
-    ),
-    tracer = tracer
-  )
 
   # If the route does not contain any wildcards we deem it specific enough to
   # qualify as the parents main route. The last route with this trait wins
@@ -73,43 +120,46 @@ with_route_ospan <- function(
     parent$set_attribute("http.route", handlerInfo$path)
   }
 
-  needs_cleanup <- TRUE
-  cleanup <- function() {
+  set_span_status <- function() {
     if (!is.null(response$status)) {
+      span <- otel::get_active_span()
+
       span$set_attribute("http.response.status_code", response$status)
+
       if (response$status >= 500) {
         span$set_status("error")
         span$set_attribute("error.type", as.character(response$status))
       }
     }
-    otel::end_span(span)
   }
-  on.exit(if (needs_cleanup) cleanup(), add = TRUE)
 
-  # Add domain to propagate the currently active span during promise context switching
-  continue <- promises::with_ospan_promise_domain({
-    otel::with_active_span(span, expr)
-  })
+  # OpenTelemetry
+  # TODO: Allow server introspection of actual server host and port (network.local.address and network.local.port)
+  # http.response.status_code and http.response.header.<key> can only be set later
+  promises::with_ospan_async(
+    name = paste0(name, "_route"),
+    options = list(kind = "server", parent = parent),
+    attributes = list2(
+      routr.route = handlerInfo$path,
+      !!!set_names(keys, paste0("routr.path.param.", names(keys)))
+    ),
+    tracer = tracer,
+    {
 
-  if (!promises::is.promising(continue)) {
-    span$set_attribute("http.response.status_code", response$status)
-    if (response$status >= 500) {
-      span$set_status("error")
-      span$set_attribute("error.type", as.character(response$status))
+      hybrid_then(
+        expr,
+        on_success = function(continue) {
+          set_span_status()
+
+          if (check_output) {
+            check_bool(continue, call = call)
+          }
+        },
+        on_failure = function(e) {
+          set_span_status()
+        },
+        tee = TRUE
+      )
     }
-    otel::end_span(span)
-    if (check_output) check_bool(continue, call = call)
-  } else {
-    needs_cleanup <- FALSE
-    continue <- promises::then(continue, function(val) {
-      if (check_output) {
-        check_bool(val, call = call)
-      }
-      val
-    })
-    continue <- promises::finally(continue, function(val) {
-      cleanup()
-    })
-  }
-  continue
+  )
 }
