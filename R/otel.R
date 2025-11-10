4c37b6df6f54407b7f7de30d749f7a46f59c76a9
Barret Schloerke <barret@posit.co>
2025-11-10 03:03:41 -0500

    chore(promises): Use `{promises}` to handle maybe-promising values (#41)

--- R/otel.R
+++ R/otel.R
@@ -1,6 +1,6 @@
 otel_tracer_name <- "r.package.routr"
 
-get_tracer <- local({
+routr_otel_tracer <- local({
   tracer <- NULL
   function() {
     if (!is.null(tracer)) {
@@ -31,40 +31,30 @@ with_route_ospan <- function(
   check_output = TRUE,
   call = caller_env()
 ) {
-  tracer <- get_tracer()
+  tracer <- routr_otel_tracer()
   is_enabled <- tracer$is_enabled()
 
   if (!is_enabled) {
-    continue <- force(expr)
-    if (!promises::is.promising(continue)) {
-      if (check_output) check_bool(continue, call = call)
-    } else {
-      continue <- promises::then(continue, function(val) {
-        if (check_output) {
-          check_bool(val, call = call)
-        }
-        val
-      })
-    }
+
+    continue <-
+      # Avoid overhead of possible promise if not utilized
+      if (check_output) {
+        promises::hybrid_then(
+          expr,
+          on_success = function(continue) {
+            check_bool(continue, call = call)
+          },
+          tee = TRUE
+        )
+      } else {
+        force(expr)
+      }
+
     return(continue)
   }
 
   name <- paste0(request$method, "_", handlerInfo$path)
-
   parent <- request$otel
-
-  # OpenTelemetry
-  # TODO: Allow server introspection of actual server host and port (network.local.address and network.local.port)
-  # http.response.status_code and http.response.header.<key> can only be set later
-  span <- otel::start_span(
-    paste0(name, "_route"),
-    options = list(kind = "server", parent = parent),
-    attributes = list2(
-      routr.route = handlerInfo$path,
-      !!!set_names(keys, paste0("routr.path.param.", names(keys)))
-    ),
-    tracer = tracer
-  )
 
   # If the route does not contain any wildcards we deem it specific enough to
   # qualify as the parents main route. The last route with this trait wins
@@ -73,43 +63,50 @@ with_route_ospan <- function(
     parent$set_attribute("http.route", handlerInfo$path)
   }
 
-  needs_cleanup <- TRUE
-  cleanup <- function() {
+  set_span_status <- function() {
     if (!is.null(response$status)) {
+      span <- otel::get_active_span()
+
       span$set_attribute("http.response.status_code", response$status)
+
       if (response$status >= 500) {
         span$set_status("error")
         span$set_attribute("error.type", as.character(response$status))
       }
     }
-    otel::end_span(span)
   }
-  on.exit(if (needs_cleanup) cleanup(), add = TRUE)
 
-  # Add domain to propagate the currently active span during promise context switching
-  continue <- promises::with_ospan_promise_domain({
-    otel::with_active_span(span, expr)
-  })
+  # OpenTelemetry
+  # TODO: Allow server introspection of actual server host and port (network.local.address and network.local.port)
+  # http.response.status_code and http.response.header.<key> can only be set later
 
-  if (!promises::is.promising(continue)) {
-    span$set_attribute("http.response.status_code", response$status)
-    if (response$status >= 500) {
-      span$set_status("error")
-      span$set_attribute("error.type", as.character(response$status))
-    }
-    otel::end_span(span)
-    if (check_output) check_bool(continue, call = call)
-  } else {
-    needs_cleanup <- FALSE
-    continue <- promises::then(continue, function(val) {
-      if (check_output) {
-        check_bool(val, call = call)
+  # Add handoff promise domain to ensure active ospan propagation
+  promises::with_otel_promise_domain({
+    # Start ospan and end when `expr` has resolved
+    promises::with_otel_span(
+      name = paste0(name, "_route"),
+      options = list(kind = "server", parent = parent),
+      attributes = list2(
+        routr.route = handlerInfo$path,
+        !!!set_names(keys, paste0("routr.path.param.", names(keys)))
+      ),
+      tracer = tracer,
+      {
+        promises::hybrid_then(
+          expr,
+          on_success = function(continue) {
+            set_span_status()
+
+            if (check_output) {
+              check_bool(continue, call = call)
+            }
+          },
+          on_failure = function(e) {
+            set_span_status()
+          },
+          tee = TRUE
+        )
       }
-      val
-    })
-    continue <- promises::finally(continue, function(val) {
-      cleanup()
-    })
-  }
-  continue
+    )
+  })
 }
